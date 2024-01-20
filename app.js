c372d1f2cde62e364ec3dad4514fc44462f53bbd
Rohit Garg <rohitgarg20021@gmail.com>
2024-01-21 00:13:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 mongoose.connect('mongodb://localhost:27017/learning_proj')
 .then(()=> console.log('connected to mongodb !!!'))
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/genres',genres);
 app.use('/api/customers',customers);
 app.use('/api/movies',movies);
+app.use('/api/rentals',rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
